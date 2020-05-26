7290078f79ce0c7d137a4385135116a03f5e030d
421953141@qq.com <421953141@qq.com>
2020-05-26 17:05:56 +0800

    init repo

---
+++ src/main/java/com/dykm/dykm/DykmApplication.java
@@ -0,0 +1,12 @@
+package com.dykm.dykm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DykmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DykmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dykm/dykm/DykmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dykm.dykm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DykmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
